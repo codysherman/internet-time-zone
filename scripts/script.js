847346caa7aee88baad8c14a72a0e8ef32e387dc
Cody Sherman <cody@codysherman.com>
2019-11-08 23:43:07 -0600

    add blinking colons

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,17 @@
 function getSwatchTime() {
   // Swatch Internet Time
   const dt = new Date();
-  document.getElementById("swatch-time").textContent = Math.floor(
-    ((((dt.getUTCHours() + 1) % 24) +
-      dt.getUTCMinutes() / 60 +
-      dt.getUTCSeconds() / 3600) *
-      1000) /
-      24
-  );
+  document.getElementById("swatch-time").textContent = (
+    Math.floor(
+      ((((dt.getUTCHours() + 1) % 24) +
+        dt.getUTCMinutes() / 60 +
+        dt.getUTCSeconds() / 3600) *
+        1000) /
+        24
+    ) / 1000
+  )
+    .toFixed(3)
+    .substr(2);
 }
 
 getSwatchTime();
@@ -22,7 +26,9 @@ function getLegacyTimes() {
   document.getElementById("local-time-hour").textContent = moment().format(
     "hh"
   );
-  document.getElementById("local-time").textContent = moment().format(":mm");
+  document.getElementById("local-time").innerHTML = moment().format(
+    `${moment().format("ss") % 2 == 0 ? ":" : "\u00A0"}mm`
+  );
   document.getElementById("local-time-meridian").textContent = moment().format(
     "a"
   );
@@ -35,7 +41,7 @@ function getLegacyTimes() {
       "local-24-hour-time-hour"
     ).textContent = moment().format("HH");
     document.getElementById("local-24-hour-time").textContent = moment().format(
-      ":mm"
+      `${moment().format("ss") % 2 == 0 ? ":" : "\u00A0"}mm`
     );
   } else if (!document.getElementById("local-24-hour-time-container").hidden) {
     document.getElementById("local-24-hour-time-container").hidden = true;
@@ -46,7 +52,7 @@ function getLegacyTimes() {
     .format("hh");
   document.getElementById("utc-time").textContent = moment()
     .utc()
-    .format(":mm");
+    .format(`${moment().format("ss") % 2 == 0 ? ":" : "\u00A0"}mm`);
   document.getElementById("utc-time-meridian").textContent = moment()
     .utc()
     .format("a");
@@ -67,7 +73,7 @@ function getLegacyTimes() {
       .format("HH");
     document.getElementById("utc-24-hour-time").textContent = moment()
       .utc()
-      .format(":mm");
+      .format(`${moment().format("ss") % 2 == 0 ? ":" : "\u00A0"}mm`);
   } else if (!document.getElementById("utc-24-hour-time-container").hidden) {
     document.getElementById("utc-24-hour-time-container").hidden = true;
   }
@@ -80,11 +86,6 @@ function showMilliseconds() {
     .slice(-3);
 }
 
-showMilliseconds();
-setInterval(() => {
-  showMilliseconds();
-}, 1);
-
 function checkMilliseconds() {
   if (
     moment()
@@ -95,6 +96,11 @@ function checkMilliseconds() {
     clearInterval(syncSeconds);
     setInterval(() => {
       getLegacyTimes();
+    }, 1000);
+    setTimeout(() => {
+      setInterval(() => {
+        showMilliseconds();
+      }, 1);
     }, 1000);
   }
 }
